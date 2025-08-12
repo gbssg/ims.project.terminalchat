63a4a7b5b6a874a40ec4e5130f7e2be03f7cd733
Yves <yves.jaros@gmail.com>
2025-08-12 15:22:35 +0200

    added testClasses, various Functions for setup, interface, grouped sections by function(not fiinished), added some comments for later use, Drawing textbox infobox and messages, but nu input yet(writing and recieving messages)

--- Code/TerminalChatV1/TerminalChatV1/Program.cs
+++ Code/TerminalChatV1/TerminalChatV1/Program.cs
@@ -1,22 +1,85 @@
-﻿using System.Collections;
-using System.Net.Sockets; 
+﻿using System.Net.Sockets;
 using System.Text;
 
 namespace TerminalChatV1
 {
+    public class Box
+    {
+        public int id;
+        public string name;
+        public bool infocus;
+    }
+    public class Tab
+    {
+        public int id; 
+        public string name;
+        public bool selected;
+    }
+    //Placeholder Message Class
+    public class Message
+    {
+        public int id;
+        public int sender;
+        public int reciever;
+        public DateTime timestamp;
+        public string body;
+    }
     internal class Program
     {
         static string serverIp;
         static int port;
-        static string[] tabs = { "TextBox", "TB1", "TB2", "TB3", "TB4" };
-        static int tabsIndex = 0;
+
+        static int boxInfocus = 0;
+        static int MaxCharV = 30;
+        static int MaxCharH = 121;
 
         static void Main()
         {
-            KeyInputThread();
-            /*
-            Connect();
+            // Create Boxes and Boxlist
+            List<Box> Boxes = new List<Box>();
+            Box textBox = new Box();
+            Boxes.Add(textBox);
 
+            Box infoBox = new Box();
+            Boxes.Add(infoBox);
+            
+            Box messageBox = new Box();
+            Boxes.Add(messageBox);
+
+            Console.WriteLine(Boxes);
+
+            // Create Tabs and Tablist
+            List<Tab> tabs = new List<Tab>();
+            Tab serverList = new Tab();
+            tabs.Add(serverList);
+
+            Tab channelList = new Tab();
+            tabs.Add(channelList);
+
+            Tab userlist = new Tab();
+            tabs.Add(userlist);
+
+            Tab notification = new Tab();
+            tabs.Add(notification);
+            Console.WriteLine(tabs);
+            Thread.Sleep(1000);
+            Setup();
+            // TODO: create Tabs
+            //Connect();
+            //KeyInputThread();
+            //Chatfunction();
+            Console.ReadLine();
+
+        }
+        static void Connect()
+        {
+            Console.Write("Gib die Server═IP ein: ");
+            serverIp = Console.ReadLine() ?? "127.0.0.1"; // Standard ist localhost
+
+            port = 5000;
+        }
+        static void Chatfunction()
+        {
             try
             {
                 using (TcpClient client = new TcpClient(serverIp, port))
@@ -60,14 +123,6 @@ namespace TerminalChatV1
                 Console.WriteLine("Fehler: " + e.Message);
                 Console.ReadLine();
             }
-            */
-        }
-        static void Connect()
-        {
-            Console.Write("Gib die Server-IP ein: ");
-            serverIp = Console.ReadLine() ?? "127.0.0.1"; // Standard ist localhost
-
-            port = 5000;
         }
         static void KeyInputThread()
         {
@@ -93,11 +148,59 @@ namespace TerminalChatV1
         }
         static void NextTab()
         {
-            if (tabsIndex >= 4) tabsIndex = 0;
-            else ++tabsIndex;
-            Console.WriteLine(tabs[tabsIndex]);
+            if (boxInfocus >= 4) boxInfocus = 0;
+            else ++boxInfocus;
+            //Console.WriteLine(tabs[boxInfocus]);
 
         }
+        static void Setup()
+        {
+            DrawTextbox();
+            DrawInfobox();
+
+        }
+        static void DrawTextbox()
+        {
+            int TBposY = MaxCharV - 6;
             
+            string TBascii = "╔═══════════════════════════════════════════════════════════════════════════════════════════════════╩══════════════════╣" +
+                           "\n║                                                                                                                      ║" +
+                           "\n║                                                                                                                      ║" +
+                           "\n║                                                                                                                      ║" +
+                           "\n╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝";
+
+            Console.SetCursorPosition(0, TBposY);
+            Console.Write(TBascii);
+
+
+        }
+        static void DrawInfobox()
+        {
+            int IBasciiLength = 22;
+            int IBasciiWidth = 21;
+            string IBasciiTOP = "╔══════════════════╗";
+            string IBasciiCenter = "║                  ║";
+            Console.SetCursorPosition(MaxCharH - IBasciiWidth, 0);
+            // Info box gets printed
+            Console.Write(IBasciiTOP);
+            for (int i = 0; i <= IBasciiLength; i++)
+            {
+                Console.SetCursorPosition(MaxCharH - IBasciiWidth, 1+i);
+                Console.Write(IBasciiCenter);
+                
+            }
+        }
+        static void DrawMessage(Message message)
+        {
+            // Class created but no media to display, not testable rn
+            Console.WriteLine($"[{message.timestamp}]{message.sender}:{message.body}");
+
+
+        }
+        static void DrawTabs()
+        {
+
+        }
+
     }
 }
