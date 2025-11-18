dcaee29e2442491fdd71cf342aeae6b75a02c359
Yvxzx <yves.jaros@gmail.com>
2025-11-18 15:41:21 +0100

    added server- dataclasses, dataCRUD, server executable, bevore setup and main function revamp

--- Code/TerminalChatV1/TerminalChatServerV1/ServerClasses.cs
+++ Code/TerminalChatV1/TerminalChatServerV1/ServerClasses.cs
@@ -1,25 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+
 
 namespace TerminalChatServer
 {
+    public class ServerList
+    {
+        public List<Server> Servers { get; } = new List<Server>();
+    }
     public class Server
     {
-        public string Ip { get; set; }
+        public string Ip { get; set; } 
         public string Port { get; set; }
         public string Name { get; set; }
-        public string description { get; set; }
-        public Guid UUID { get; set; }
+        public string Description { get; set; }
+        public Guid UUID { get; set; } = Guid.CreateVersion7();
+        public List<Channel> Channels { get; set; } 
+
+        public Server(string _name, string _description, string _port , List<Channel> _channels) 
+        { 
+            this.Ip = GetLocalIpAddress();
+            this.Port = _port;
+            this.Name = _name;
+            this.Description = _description;
+            this.Channels = _channels;
+        }
+
+        public void UpdateChannel(Channel _channel)
+        {
+            int index = Channels.FindIndex(ch => ch.UUID == _channel.UUID);
+            if (index != -1)
+            {
+                Channels[index] = _channel;
+            } else
+            {
+                Channels.Add(_channel);
+            }
+        }
+        public string GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No Network adapters with an IPv4 address in the system!");
+        }
     }
 
     public class Channel
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public Guid UUID { get; set; }
+        public Guid UUID { get; } = Guid.CreateVersion7();
+        public Channel(string _name, string _description)
+        {
+            this.Name = _name;
+            this.Description = _description;
+        }
 
     }
 }
---
+++ Code/TerminalChatV1/TerminalChatServerV1/dataCrud.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TerminalChatServer
+{
+    public class dataCrud
+    {
+        public string directoryPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TerminalChatServer");
+        public string serverListPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TerminalChatServer", "servers.json");
+
+        public void SetupAppDir()
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            if (!File.Exists(serverListPath))
+            {
+                using (File.Create(serverListPath)) { };
+            }
+        }
+        public ServerList GetServers()
+        {
+            string jsonText = File.ReadAllText(serverListPath);
+
+            ServerList serverList = JsonSerializer.Deserialize<ServerList>(jsonText);
+
+            return serverList;
+        }
+
+        public void AddServer(Server _server)
+        {
+            ServerList serverList = GetServers();
+            serverList.Servers.Add(_server);
+
+            var opt = new JsonSerializerOptions { WriteIndented = true };
+            string jsonText = JsonSerializer.Serialize(serverList);
+
+            File.WriteAllText(serverListPath, jsonText);
+        }
+        public void DeleteServer(Server _server)
+        {
+            ServerList serverList = GetServers();
+            serverList.Servers.Remove(_server);
+
+            var opt = new JsonSerializerOptions { WriteIndented = true };
+            string jsonText = JsonSerializer.Serialize(serverList);
+
+            File.WriteAllText(serverListPath, jsonText);
+        }
+
+        public void UpdateServer(Server _server)
+        {
+            ServerList serverList = GetServers();
+
+            int index = serverList.Servers.FindIndex(sv => sv.UUID == _server.UUID);
+            if (index != -1)
+            {
+                serverList.Servers[index] = _server;
+            } else
+            {
+                AddServer(_server);
+                //Some sort of Warning! To be implemented! not mvp
+            }
+
+            var opt = new JsonSerializerOptions { WriteIndented = true };
+            string jsonText = JsonSerializer.Serialize(serverList);
+
+            File.WriteAllText(serverListPath, jsonText);
+
+        }
+        public Server GetServer(Guid _UUID)
+        {
+            ServerList serverList = GetServers();
+            int index = serverList.Servers.FindIndex(sv =>sv.UUID == _UUID);
+            if (index != -1)
+            {
+                return serverList.Servers[index];
+            } else
+            {
+                throw new Exception("Could not find Server identified by this UUID!");
+                //Some sort of ErrorHandling
+            }
+        }
+    }
+}
