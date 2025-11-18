682ff3ebb8bb189c65d776dc5e62f598eccfd73c
Yvxzx <yves.jaros@gmail.com>
2025-11-18 13:11:37 +0100

    revamped serverdataclasses

---
+++ Code/TerminalChatV1/TerminalChatServerV1/ServerClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalChatServer
+{
+    public class Server
+    {
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public Guid UUID { get; set; }
+    }
+
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid UUID { get; set; }
+
+    }
+}
