e41e5ec97e1f54dfb867c99ccf40f52d1e0f1f4e
Yves <yves.jaros@gmail.com>
2026-02-17 13:21:36 +0100

    Repo cleanup: removed redundant files and renewed naming

---
+++ Code/TerminalChatV1/TerminalChatServerV1/ServerTcpConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TerminalChatServer
+{
+    public class ServerTcpConnection
+    {
+        public void RunServer(Server _server)
+        {
+            
+            IPAddress ipAddr = IPAddress.Parse( _server.Ip );
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, _server.Port);
+
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(localEndPoint);
+            listener.Listen(1000);
+
+        }
+    }
+}
